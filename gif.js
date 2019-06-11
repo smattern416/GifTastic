137cdf6c943a465e83ca0e6c3ed32701ba552bc1
Stephanie Mattern <smattern416@gmail.com>
2019-06-11 07:36:45 -0400

    added queryURL within functions for images

--- gif.js
+++ gif.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
     // variables
     var topics = ["Survivor", "The Bachelor", "Love Island"];
+
+    // function for displaying the images
+
+    function images() {
+        $("#images").empty();
+        var input = $(this).attr("data");
+        var limit = 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=aD1fo0UXpTYcfSTNLvmDA6t3PUeZRwyR";
+    }
+
+
 )}
